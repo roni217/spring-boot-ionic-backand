74263a4d75bcfa2e1caf1f9d91d8e9c2ac86bfa9
ronaldo akio <roni.217@gmail.com>
2018-03-27 22:13:48 -0300

    REST testado

---
+++ src/main/java/br/nom/rakio/prjmc/resources/CategoriaResource.java
@@ -0,0 +1,18 @@
+package br.nom.rakio.prjmc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "rest esta funcionando";
+	}
+	
+	
+	
+}
--- src/main/java/br/nom/rakio/prjmc/resources/PrjmcApplication.java
+++ src/main/java/br/nom/rakio/prjmc/resources/PrjmcApplication.java
@@ -1,4 +1,4 @@
-package br.nom.rakio.example.demo;
+package br.nom.rakio.prjmc.resources;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
