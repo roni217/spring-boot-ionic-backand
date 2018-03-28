a69f52080f570f239501b8114f6e5183ef8b6ec5
ronaldo akio <roni.217@gmail.com>
2018-03-27 21:56:08 -0300

    Projeto criado

---
+++ src/main/java/br/nom/rakio/example/demo/PrjmcApplication.java
@@ -0,0 +1,12 @@
+package br.nom.rakio.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjmcApplication.class, args);
+	}
+}
