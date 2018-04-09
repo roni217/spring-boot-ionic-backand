40e7ab66c658bdce50cbebbf7dd263b3d92f55d3
Ronaldo Akio <roni.217@gmail.com>
2018-04-08 22:23:15 -0300

    Criando operacao de instanciacao

--- src/main/java/br/nom/rakio/prjmc/PrjmcApplication.java
+++ src/main/java/br/nom/rakio/prjmc/PrjmcApplication.java
@@ -1,12 +1,31 @@
 package br.nom.rakio.prjmc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PrjmcApplication {
+import br.nom.rakio.prjmc.domain.Categoria;
+import br.nom.rakio.prjmc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class PrjmcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository; 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PrjmcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
