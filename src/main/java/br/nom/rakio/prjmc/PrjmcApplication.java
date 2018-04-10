496c4f686ce35b43db38f8e6cd385c390ee71728
Ronaldo Akio <roni.217@gmail.com>
2018-04-09 22:44:57 -0300

    Produto e associacao muitos para muitos

--- src/main/java/br/nom/rakio/prjmc/PrjmcApplication.java
+++ src/main/java/br/nom/rakio/prjmc/PrjmcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.nom.rakio.prjmc.domain.Categoria;
+import br.nom.rakio.prjmc.domain.Produto;
 import br.nom.rakio.prjmc.repositories.CategoriaRepository;
+import br.nom.rakio.prjmc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class PrjmcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository; 
+
+	@Autowired
+	private ProdutoRepository produtoRepository; 
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PrjmcApplication.class, args);
@@ -26,6 +31,18 @@ public class PrjmcApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.0);
+		Produto p2 = new Produto(null, "Impressora", 800.0);
+		Produto p3 = new Produto(null, "Mouse", 80.0);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 }
--- src/main/java/br/nom/rakio/prjmc/repositories/ProdutoRepository.java
+++ src/main/java/br/nom/rakio/prjmc/repositories/ProdutoRepository.java
@@ -3,9 +3,9 @@ package br.nom.rakio.prjmc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import br.nom.rakio.prjmc.domain.Categoria;
+import br.nom.rakio.prjmc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-	
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
 }
