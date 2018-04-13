5dafdff00341551fe577dec37e5a571dcf2a25e3
Ronaldo Akio <roni.217@gmail.com>
2018-04-12 21:46:32 -0300

    Endpoint /clientes/id criado

---
+++ src/main/java/br/nom/rakio/prjmc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package br.nom.rakio.prjmc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.nom.rakio.prjmc.domain.Cliente;
+import br.nom.rakio.prjmc.repositories.ClienteRepository;
+import br.nom.rakio.prjmc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+
+	}
+
+}
