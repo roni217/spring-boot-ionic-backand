ec58b603127592b2b30588c23f014749937d6cd9
Ronaldo Akio <roni.217@gmail.com>
2018-04-22 11:14:37 -0300

    Integracao com o travicCI

--- src/test/java/br/nom/rakio/example/demo/PrjmcApplicationTests.java
+++ src/test/java/br/nom/rakio/example/demo/PrjmcApplicationTests.java
@@ -1,16 +1,44 @@
 package br.nom.rakio.example.demo;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.nom.rakio.prjmc.domain.Categoria;
+import br.nom.rakio.prjmc.services.CategoriaService;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = CategoriaService.class)
 public class PrjmcApplicationTests {
 
+	@MockBean
+	private CategoriaService service;
+	private	Categoria categoria = new Categoria();
+	
+	
+	@Before
+	public void setUp() {
+
+	}
+	
 	@Test
 	public void contextLoads() {
+		categoria.setId(1);
+		categoria.setNome("rei");
+		Mockito.when(service.buscar(1)).thenReturn(categoria);
+		Categoria findById = service.buscar(1);
+		
+		String nome = findById.getNome();
+		System.out.println(nome);
+		assertEquals("rei", nome);
 	}
 
 }
