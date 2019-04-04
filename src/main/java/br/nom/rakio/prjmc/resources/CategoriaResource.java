8f0b46803937aad31bcf22a344bd8bda6d520866
Ronaldo <roni.217@gmail.com>
2019-04-03 23:02:13 -0300

    validacao sintatica com Bean validation

--- src/main/java/br/nom/rakio/prjmc/resources/CategoriaResource.java
+++ src/main/java/br/nom/rakio/prjmc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -34,14 +36,15 @@ public class CategoriaResource {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
-		obj = service.insert(obj);
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO) {
+		Categoria obj = service.fromDTO(objDTO);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> Update(@RequestBody Categoria obj, @PathVariable Integer id) {
+	public ResponseEntity<Void> Update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id) {
+		Categoria obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
--- src/main/java/br/nom/rakio/prjmc/services/CategoriaService.java
+++ src/main/java/br/nom/rakio/prjmc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import br.nom.rakio.prjmc.domain.Categoria;
+import br.nom.rakio.prjmc.dto.CategoriaDTO;
 import br.nom.rakio.prjmc.repositories.CategoriaRepository;
 import br.nom.rakio.prjmc.services.exceptions.DataIntegrityException;
 import br.nom.rakio.prjmc.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
