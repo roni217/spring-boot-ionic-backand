4195f2f8781704831351103c676884d7732f0c07
Ronaldo <roni.217@gmail.com>
2019-04-17 00:05:10 -0300

    Busca de produtos por nome e categorias paginado

--- src/main/java/br/nom/rakio/prjmc/repositories/ProdutoRepository.java
+++ src/main/java/br/nom/rakio/prjmc/repositories/ProdutoRepository.java
@@ -1,11 +1,22 @@
 package br.nom.rakio.prjmc.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.nom.rakio.prjmc.domain.Categoria;
 import br.nom.rakio.prjmc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
+	//@Transactional(readOnly=true)
+//	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+//	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+	
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
 }
