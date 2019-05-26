f471010c02e8258ae3d3211678ef807dbb093b1c
RONi_ <RONi_@DESKTOP-MIM9B5B>
2019-05-26 11:01:43 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/br/nom/rakio/prjmc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package br.nom.rakio.prjmc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.nom.rakio.prjmc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instanciateDatabase() throws ParseException {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+			
+		dbService.instanciateTesteDatabase();
+		return true;
+	}
+}
