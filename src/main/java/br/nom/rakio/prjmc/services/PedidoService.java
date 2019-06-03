c9625aeb1cbb789cf9801949b8f87bf34a8016d3
RONi_ <RONi_@DESKTOP-MIM9B5B>
2019-06-02 21:48:08 -0300

    Implementando toString de Pedido

--- src/main/java/br/nom/rakio/prjmc/services/PedidoService.java
+++ src/main/java/br/nom/rakio/prjmc/services/PedidoService.java
@@ -29,7 +29,10 @@ public class PedidoService {
 
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-
+	
+	@Autowired
+	private ClienteService clienteService;
+	
 	@Autowired
 	private ProdutoService produtoService;
 	public Pedido buscar(Integer id) {
@@ -41,6 +44,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstant(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -51,10 +55,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
