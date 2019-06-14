b757be4900bf50215fe6995cd9e8f713419f9351
RONi_ <RONi_@DESKTOP-MIM9B5B>
2019-06-13 23:11:16 -0300

    comentado AbstractEmailService para perfil test

--- src/main/java/br/nom/rakio/prjmc/services/AbstractEmailService.java
+++ src/main/java/br/nom/rakio/prjmc/services/AbstractEmailService.java
@@ -2,16 +2,8 @@ package br.nom.rakio.prjmc.services;
 
 import java.util.Date;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 
 import br.nom.rakio.prjmc.domain.Pedido;
 
@@ -19,12 +11,12 @@ public abstract class AbstractEmailService implements EmailService {
 
 	@Value("${default.sender}")
 	private String sender;
-
-	@Autowired
-	private TemplateEngine templateEngine;
-
-	@Autowired
-	private JavaMailSender javaMailSender;
+//TODO: o codigo esta comentado para evitar conflito em TEST, resolver depois
+//	@Autowired
+//	private TemplateEngine templateEngine;
+//
+//	@Autowired
+//	private JavaMailSender javaMailSender;
 	
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
@@ -42,32 +34,32 @@ public abstract class AbstractEmailService implements EmailService {
 		return sm;
 	}
 
-	protected String htmlFromTemplatePedido(Pedido obj) {
-		Context context = new Context();
-		context.setVariable("pedido", obj);
-		return templateEngine.process("email/confirmacaoPedido", context);
-	}
+//	protected String htmlFromTemplatePedido(Pedido obj) {
+//		Context context = new Context();
+//		context.setVariable("pedido", obj);
+//		return templateEngine.process("email/confirmacaoPedido", context);
+//	}
 
-	@Override
-	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
-		try {
-			MimeMessage mm = prepareMimeMessageFromPedido(obj);
-			sendHtmlEmail(mm);
-		}
-		catch (MessagingException e) {
-			sendOrderConfirmationEmail(obj);
-		}
-	}
+//	@Override
+//	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
+//		try {
+//			MimeMessage mm = prepareMimeMessageFromPedido(obj);
+//			sendHtmlEmail(mm);
+//		}
+//		catch (MessagingException e) {
+//			sendOrderConfirmationEmail(obj);
+//		}
+//	}
 
-	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
-		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
-		mmh.setTo(obj.getCliente().getEmail());
-		mmh.setFrom(sender);
-		mmh.setSubject("Pedido confirmado! Código: " + obj.getId());
-		mmh.setSentDate(new Date(System.currentTimeMillis()));
-		mmh.setText(htmlFromTemplatePedido(obj), true);
-		return mimeMessage;
-	}
+//	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException {
+//		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+//		MimeMessageHelper mmh = new MimeMessageHelper(mimeMessage, true);
+//		mmh.setTo(obj.getCliente().getEmail());
+//		mmh.setFrom(sender);
+//		mmh.setSubject("Pedido confirmado! Código: " + obj.getId());
+//		mmh.setSentDate(new Date(System.currentTimeMillis()));
+//		mmh.setText(htmlFromTemplatePedido(obj), true);
+//		return mimeMessage;
+//	}
 
 }
